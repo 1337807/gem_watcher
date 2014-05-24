4303ea73dc3f776d7723c3e5095862d31009f427
Jonan Scheffler <jonanscheffler@gmail.com>
2014-05-24 03:19:47 -0700

    Validate presence of either a body or a url.
    Validate presence of either a body or a url.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,13 @@
 class List < ActiveRecord::Base
   belongs_to :user
+
+  validate :either_body_or_url
+
+  private
+
+  def either_body_or_url
+    if [self.body, self.url].reject(&:blank?).empty?
+      errors[:base] << ("You must include either a list of gems or a file where we can extract the list.")
+    end
+  end
 end
