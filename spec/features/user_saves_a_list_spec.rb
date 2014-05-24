e7fc95ff3de4756470af49c215aca2b1e9413d82
Jonan Scheffler <jonanscheffler@gmail.com>
2014-05-24 03:18:52 -0700

    Expand features for saving a list.
    Expand features for saving a list.

--- spec/features/user_saves_a_list_spec.rb
+++ spec/features/user_saves_a_list_spec.rb
@@ -1,16 +1,44 @@
 require 'spec_helper'
 
 feature 'User saves a list' do
-  before :all do
+  before :each do
     sign_up
   end
 
-  scenario 'with body and no url' do
+  scenario 'without a body or a url' do
+    visit root_path
+    click_button 'Save'
+
+    expect(page).to have_content('You must include either a list of gems or a file where we can extract the list.')
+  end
+
+  scenario 'with a body and without a url' do
     body = Faker::Lorem.words(10).join("\r")
     visit root_path
     fill_in :list_body, with: body
     click_button 'Save'
 
     expect(List.last.body).to eql(body)
+  end
+
+  scenario 'with a url and without a body' do
+    url = Faker::Internet.url
+    visit root_path
+    fill_in :list_url, with: url
+    click_button 'Save'
+
+    expect(List.last.url).to eql(url)
+  end
+
+  scenario 'with a body and a url' do
+    body = Faker::Lorem.words(10).join("\r")
+    url = Faker::Internet.url
+    visit root_path
+    fill_in :list_body, with: body
+    fill_in :list_url, with: url
+    click_button 'Save'
+
+    expect(List.last.body).to eql(body)
+    expect(List.last.url).to eql(url)
   end
 end
