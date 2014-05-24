69f5f4e73ef798987d69c6b90a8a7b1c7603c42b
Jonan Scheffler <jonanscheffler@gmail.com>
2014-05-24 03:19:23 -0700

    Test list validation.
    Test list validation.

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,4 +1,26 @@
 require 'spec_helper'
 
 describe List do
+  let(:body) { Faker::Lorem.words(10).join("\r") }
+  let(:url) { Faker::Internet.url }
+
+  it "can be created with a url and without a body" do
+    List.create(url: url)
+    expect(List.last.url).to eql(url)
+  end
+
+  it "can be created with a body and without a url" do
+    List.create(body: body)
+    expect(List.last.body).to eql(body)
+  end
+
+  it "can be created with both a body and a url" do
+    List.create(body: body, url: url)
+    expect(List.last.url).to eql(url)
+    expect(List.last.body).to eql(body)
+  end
+
+  it "can't be created without either a body or a url" do
+    List.create
+  end
 end
