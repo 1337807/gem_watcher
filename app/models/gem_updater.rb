de43dc8a5a88020a79b5ef34ab5abbe817ebd9cb
Jonan Scheffler <jonanscheffler@gmail.com>
2014-05-26 11:23:38 -0700

    Gem updaters can fetch gem versions.
    Gem updaters can fetch gem versions.

--- app/models/gem_updater.rb
+++ app/models/gem_updater.rb
@@ -1,4 +1,15 @@
 class GemUpdater
-  def self.update_gems!
+  def self.update_gem_versions!
+    new.update_gem_versions!
+  end
+
+  def initialize(service = nil)
+    @service = service || GemService.new
+  end
+
+  def update_gem_versions!
+    Jem.all.each do |gem|
+      @service.fetch_version(gem.name)
+    end
   end
 end
--- spec/models/gem_updater_spec.rb
+++ spec/models/gem_updater_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe GemUpdater do
-  it "updates gems using the gem service"
+  before :all do
+    @service = GemService.new
+  end
+
+  it "fetches a version for each gem" do
+    Fabricate(:jem, name: 'rake', version: '0.1')
+    Fabricate(:jem, name: 'rack', version: '0.1')
+    @service.should_receive(:fetch_version).with('rake')
+    @service.should_receive(:fetch_version).with('rack')
+    GemUpdater.new(@service).update_gem_versions!
+  end
 end
